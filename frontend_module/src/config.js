1675bdc23b77531db552fe4ac580b4f06b0fd7f4
Sibasishnandy <sibasishnandy@gmail.com>
2026-03-22 22:01:26 +0530

    again updation

--- frontend_module/src/config.js
+++ frontend_module/src/config.js
@@ -1,2 +1,4 @@
-export const SERVER = import.meta.env.VITE_SERVER || "http://localhost:5000";
+// src/config.js
+export const SERVER = "https://community-module-fullstack-1.onrender.com/";
+
 export const EMOJIS = ["👍", "❤️", "😂", "😮", "😢", "🔥", "👏", "🎉"];
